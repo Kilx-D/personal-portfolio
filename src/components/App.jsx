18c5244e95153a27a3cf47c7b7b4d4b0458246ff
Kilx-D <kelxwii@gmail.com>
2022-04-13 17:16:21 -0400

    added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Header from './header/Header';
+import Nav from './nav/Nav';
+import About from './about/About';
+import Portfolio from './portfolio/Portfolio';
+import Contact from './contact/Contact';
+import Footer from './footer/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Portfolio />
+    <Contact />
+    <Footer />
+    </>
+    )
 }
 
 export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CTA from './CTA';
+import './header.css';
+import ME from '../../assets/ivy.jpg';
+import HeaderSocials from './HeaderSocials';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container header_container">
+            <h5>Hello I'm</h5>
+            <h1>Daniel Portillo</h1>
+            <h5 className="text-light">Fullstack Web Developer</h5>
+            <CTA />
+            <HeaderSocials />
+
+            <div className="me">
+                <img src={ME} alt="me" />
+                
+                <a href="container" className='scroll_down'>Scroll Down</a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
