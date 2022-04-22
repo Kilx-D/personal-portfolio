e7a0d7bc01317814a0d68bbd6d48edb2162f1143
kilxter <kelxwii@gmail.com>
2022-04-21 20:16:32 -0400

    added the about me section and finished on the nav bar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './about.css';
+import ME from '../../assets/me-about.jpg';
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className="about_me_image">
+            <img src={ME} alt="about image" />
+          </div>
+        </div>
+
+        <div className='about_content'>
+          <div className="about_cards">
+            <article className='about_card'>
+              <h5></h5>
+            </article>
+          </div>
+
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,14 +3,18 @@ import {AiOutlineHome} from 'react-icons/ai';
 import {AiOutlineUser} from 'react-icons/ai';
 import {BiBook} from 'react-icons/bi';
 import {BiMessageSquareDetail} from 'react-icons/bi';
+import './nav.css'
+import {useState} from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#portfolio"><BiBook /></a>
-      <a href="#contact"><BiMessageSquareDetail /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiBook /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
 
     </nav>
   )
