46ea002cfa22f80babd9ffc17fb88a27efde2cd5
kilxter <kelxwii@gmail.com>
2022-05-04 01:59:46 -0400

    added analytics and trackers and fixed the font not loading in

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import React from 'react';
 import App from './components/App';
 import './index.css'
 
+fetch("https://user-analyzer.herokuapp.com/analyze")
+
 ReactDOM.render(<App />, document.getElementById("root"))
